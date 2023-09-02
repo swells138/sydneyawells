e7fff2eee05b07a458d92e881dd087c04952eb2f
swells <sydneywells103@gmail.com>
2023-09-02 18:55:40 -0400

    initial commit

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="columns-2">
+        <div>
+         anchor
+        </div>
+        <div className="columns-3">
+          <h1>Home</h1>
+          <h1>Blog</h1>
+          <h1>Code</h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Navbar
