1c237f4cbe30ff8ac86c5ed3b046cd6c64871510
swells <sydneywells103@gmail.com>
2023-09-05 18:19:30 -0400

    hero page complete

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
-
+import Image from 'next/image'
+import logo from '/home/swells/portfolio/mywebsite/sydneyawells/src/app/Untitled.png'
 const Navbar = () => {
   return (
     <>
+    <div className='bg-gradient-to-r from-sky-950 via-slate-600 to-stone-800 '>
       <div className="columns-2">
-        <div>
-         anchor
-        </div>
+      <Image
+        src={logo}
+        width={200}
+        height={200}
+        alt="Picture of the author"
+      />
         <div className="columns-3">
-          <h1>Home</h1>
-          <h1>Blog</h1>
-          <h1>Code</h1>
+          <h1 className='font-semibold text-slate-900'>Home</h1>
+          <h1 className='font-semibold text-slate-900'>Blog</h1>
+          <h1 className='font-semibold text-slate-900'>Code</h1>
         </div>
       </div>
+    </div>
     </>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,48 @@
 import Image from 'next/image'
-import Navbar from './components/Navbar'
-
-
+import logo from '/home/swells/portfolio/mywebsite/sydneyawells/src/app/black logo.png'
+import squiggles from "/home/swells/portfolio/mywebsite/sydneyawells/src/app/squiggle bg.png"
 export default function Home() {
   return (
     <>
-      <Navbar></Navbar>
-
-      <div className='flex flex-col items-center pt-10'>
-        <h1 className="text-8xl text-center font-mono">Hi. I'm Sydney</h1>
-        <div className="w-3/4">
-          <p>I am new to tech and I am not sure what to write here but we are going to fill it in with some fun stuff so that I can see what the layout of this page is going to be when I start to add content.</p>
+      <div className='columns-2 flex justify-between'>
+        <div>
+          <Image
+            src={logo}
+            width={200}
+            height={200}
+            alt="Picture of the author"
+          />
+        </div>
+        <div className='pt-10 pr-12'>
+          <div className="columns-3">
+            <h1 className='font-semibold text-slate-900'>About</h1>
+            <h1 className='font-semibold text-slate-900'>Resume</h1>
+            <h1 className='font-semibold text-slate-900'>Code</h1>
+          </div>
         </div>
       </div>
-
+      <div className='flex flex-col items-center pt-12 mt-12'>
+        <div className='grid grid-rows-3 grid-flow-col gap-4'>
+          <div className='row-span-3'>
+            <h1 className="text-9xl font-mono font-black">Sydney
+              Wells</h1>
+          </div>
+          <div className='col-span-2'>
+            <p className='font-semibold leading-loose text-3xl'>Full Stack Web Developer</p>
+          </div>
+          <div className="row-span-2 col-span-2">
+            <p className='font-medium text-2xl'>Technical Solutions Consultant</p>
+          </div>
+        </div>
+        {/* <div className='w-3/4'>
+          <p className='indent-4'>This could be a summary of shit that I am up to or something like that. I like games and dun and crafts and doing things that make my buns wiggle. Some poo about the navy and doing cool boat stuff and being passionate about junk and such. Keep making this long so that you can see what it looks like when there is content here.</p>
+        </div> */}
+      </div>
+      <Image
+        src={squiggles}
+        fill={true}
+        alt="Picture of the author"
+      />
     </>
   )
 }
